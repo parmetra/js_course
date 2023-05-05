9e5b5da6642686d2c232f0379361a3c35b287e59
Artyom <artem-2948@yandex.ru>
2023-05-05 14:00:48 +0300

    Урок 19. Правктика, ч.1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 "use srict";
 
-let incr = 10;
-let decr = 10;
-// ++incr;
-console.log(++incr);
+let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+let lastWatchedFilm = prompt("Последний просмотренный фильм?", "");
+let yourRatingOfLastWatchedFilm = +prompt("Оценка последнего просмотренного фильма?", "");
+personalMovieDB.movies[lastWatchedFilm] = yourRatingOfLastWatchedFilm;
+
+lastWatchedFilm = prompt("Последний просмотренный фильм?", "");
+yourRatingOfLastWatchedFilm = +prompt("Оценка последнего просмотренного фильма?", "");
+personalMovieDB.movies[lastWatchedFilm] = yourRatingOfLastWatchedFilm;
+
+console.log(personalMovieDB);
